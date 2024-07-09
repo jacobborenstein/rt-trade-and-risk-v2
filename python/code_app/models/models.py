457b370927f6e0d579f21f59b28a3c6b462e9e9a
Bobojacob <jborens2@mail.yu.edu>
2024-07-09 16:52:27 -0400

    models.py
    added a to_json method for Positions

---
+++ python/code_app/models/models.py
@@ -0,0 +1,49 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+from enum import Enum
+from typing import Literal, List
+
+class Direction(str, Enum):
+    BUY = 'BUY'
+    SELL = 'SELL'
+
+class Account(BaseModel):
+    account_id: str = Field(..., alias="accountId")
+    account_name: str = Field(..., alias="accountName")
+    trades: List['Trade'] = []
+    def get_account_id(self) -> str:
+        return self.account_id
+
+    def get_account_name(self) -> str:
+        return self.account_name
+
+
+class PrimaryKey(BaseModel):
+    account: Account
+    trade_id: str = Field(..., alias="tradeId")
+
+class Trade(BaseModel):
+    primaryKey: PrimaryKey
+    ticker: str
+    direction: Direction
+    quantity: int
+    executed_price: float = Field(..., alias="executedPrice")
+    executed_user: str = Field(..., alias="executedUser")
+    executed_time: datetime = Field(..., alias="executedTime")
+
+class Position(BaseModel):
+    account: Account
+    ticker: str
+    quantity: int
+    avg_price: float = Field(..., alias="avgPrice")
+    last_updated: datetime = Field(..., alias="lastUpdated")
+
+    def to_json(self):
+    # Convert the object to a dictionary
+        return {
+            "account": self.account.dict(by_alias=True),
+            "ticker": self.ticker,
+            "quantity": self.quantity,
+            "avgPrice": self.avg_price,
+            "lastUpdated": self.last_updated.isoformat() if self.last_updated else None
+        }
